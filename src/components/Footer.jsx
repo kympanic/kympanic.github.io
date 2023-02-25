4f43b263ddf5c5aa2d54b95866b74c925fc047bf
Daniel Yoo <dacapo0509@gmail.com>
2023-02-25 13:17:51 -0800

    editing project section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
 const Footer = ({ classicHeader, darkTheme, handleNavClick }) => {
-  return (
-    <footer
-      id="footer"
-      className={"section " + (darkTheme ? "footer-dark bg-dark-1" : "")}
-    >
-      <div className={"container " + (classicHeader ? "" : "px-lg-5")}>
-        <div className="row">
-          <div className="col-lg-6 text-center text-lg-start">
-            <p className="mb-3 mb-lg-0">
-              Copyright © 2023{" "}
-              <a
-                href="#home"
-                className="fw-500"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleNavClick("home");
-                }}
-              >
-                Simone
-              </a>
-              . All Rights Reserved.
-            </p>
-          </div>
-          <div className="col-lg-6">
+	return (
+		<footer
+			id="footer"
+			className={"section " + (darkTheme ? "footer-dark bg-dark-1" : "")}
+		>
+			<div className={"container " + (classicHeader ? "" : "px-lg-5")}>
+				<div className="row">
+					<div className="col-lg-6 text-center text-lg-start">
+						<p className="mb-3 mb-lg-0">
+							Copyright © 2023{" "}
+							<a
+								href="#home"
+								className="fw-500"
+								onClick={(e) => {
+									e.preventDefault();
+									handleNavClick("home");
+								}}
+							>
+								Daniel Yoo
+							</a>
+							. All Rights Reserved.
+						</p>
+					</div>
+					{/* <div className="col-lg-6">
             <ul className="nav nav-separator justify-content-center justify-content-lg-end">
               <li className="nav-item">
                 <a
@@ -47,11 +47,11 @@ const Footer = ({ classicHeader, darkTheme, handleNavClick }) => {
                 </a>
               </li>
             </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+          </div> */}
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
