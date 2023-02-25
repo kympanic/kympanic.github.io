46758aa15f6496d0680e8b414aad95dae65433c6
Daniel Yoo <dacapo0509@gmail.com>
2023-02-25 11:39:35 -0800

    rid of testimonials

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import AboutUs from "./components/About";
 import Services from "./components/Services";
 import Resume from "./components/Resume";
 import Portfolio from "./components/Portfolio";
-import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { useEffect, useState } from "react";
@@ -17,114 +16,114 @@ import PreLoader from "./components/Preloader";
 import { Tooltip } from "./components/Tooltip";
 
 function App() {
-  const classicHeader = commonConfig.classicHeader;
-  const darkTheme = commonConfig.darkTheme;
+	const classicHeader = commonConfig.classicHeader;
+	const darkTheme = commonConfig.darkTheme;
 
-  const handleNavClick = (section) => {
-    document.getElementById(section).scrollIntoView({ behavior: "smooth" });
-  };
+	const handleNavClick = (section) => {
+		document.getElementById(section).scrollIntoView({ behavior: "smooth" });
+	};
 
-  const [scrollTopVisible, setScrollTopVisible] = useState(false);
-  const [isLoading, setisLoading] = useState(true);
-  useEffect(() => {
-    const loadingTimeout = setTimeout(() => {
-      setisLoading(false);
-    }, 1000);
-    return () => {
-      clearTimeout(loadingTimeout);
-    };
-  }, []);
+	const [scrollTopVisible, setScrollTopVisible] = useState(false);
+	const [isLoading, setisLoading] = useState(true);
+	useEffect(() => {
+		const loadingTimeout = setTimeout(() => {
+			setisLoading(false);
+		}, 1000);
+		return () => {
+			clearTimeout(loadingTimeout);
+		};
+	}, []);
 
-  const checkScrollTop = () => {
-    let scrollTopBtn = document.getElementById("back-to-top");
+	const checkScrollTop = () => {
+		let scrollTopBtn = document.getElementById("back-to-top");
 
-    if (scrollTopBtn) {
-      if (
-        document.body.scrollTop > 400 ||
-        document.documentElement.scrollTop > 400
-      ) {
-        setScrollTopVisible(true);
-      } else {
-        setScrollTopVisible(false);
-      }
-    }
-  };
+		if (scrollTopBtn) {
+			if (
+				document.body.scrollTop > 400 ||
+				document.documentElement.scrollTop > 400
+			) {
+				setScrollTopVisible(true);
+			} else {
+				setScrollTopVisible(false);
+			}
+		}
+	};
 
-  if (typeof window !== "undefined") {
-    window.addEventListener("scroll", checkScrollTop);
-  }
+	if (typeof window !== "undefined") {
+		window.addEventListener("scroll", checkScrollTop);
+	}
 
-  return (
-    <>
-      <div
-        style={{ position: "relative" }}
-        className={classicHeader ? "" : "side-header"}
-      >
-        {isLoading && <PreLoader></PreLoader>}
+	return (
+		<>
+			<div
+				style={{ position: "relative" }}
+				className={classicHeader ? "" : "side-header"}
+			>
+				{isLoading && <PreLoader></PreLoader>}
 
-        <div id="main-wrapper">
-          {classicHeader ? (
-            <ClassicHeader handleNavClick={handleNavClick}></ClassicHeader>
-          ) : (
-            <Header handleNavClick={handleNavClick}></Header>
-          )}
+				<div id="main-wrapper">
+					{classicHeader ? (
+						<ClassicHeader
+							handleNavClick={handleNavClick}
+						></ClassicHeader>
+					) : (
+						<Header handleNavClick={handleNavClick}></Header>
+					)}
 
-          <div id="content" role="main">
-            <Home
-              classicHeader={classicHeader}
-              darkTheme={darkTheme}
-              handleNavClick={handleNavClick}
-            ></Home>
-            <AboutUs
-              classicHeader={classicHeader}
-              darkTheme={darkTheme}
-            ></AboutUs>
-            <Services
-              classicHeader={classicHeader}
-              darkTheme={darkTheme}
-            ></Services>
-            <Resume
-              classicHeader={classicHeader}
-              darkTheme={darkTheme}
-            ></Resume>
-            <Portfolio
-              classicHeader={classicHeader}
-              darkTheme={darkTheme}
-            ></Portfolio>
-            <Testimonials
-              classicHeader={classicHeader}
-              darkTheme={darkTheme}
-            ></Testimonials>
-            <Contact
-              classicHeader={classicHeader}
-              darkTheme={darkTheme}
-            ></Contact>
-          </div>
-          <Footer
-            classicHeader={classicHeader}
-            darkTheme={darkTheme}
-            handleNavClick={handleNavClick}
-          ></Footer>
-        </div>
-        {/* back to top */}
-        <Tooltip text="Back to Top" placement="left">
-          <span
-            id="back-to-top"
-            className="rounded-circle"
-            style={{ display: scrollTopVisible ? "inline" : "none" }}
-            onClick={() => {
-              window.scrollTo({ top: 0, behavior: "smooth" });
-            }}
-          >
-            <i className="fa fa-chevron-up"></i>
-          </span>
-        </Tooltip>
+					<div id="content" role="main">
+						<Home
+							classicHeader={classicHeader}
+							darkTheme={darkTheme}
+							handleNavClick={handleNavClick}
+						></Home>
+						<AboutUs
+							classicHeader={classicHeader}
+							darkTheme={darkTheme}
+						></AboutUs>
+						<Services
+							classicHeader={classicHeader}
+							darkTheme={darkTheme}
+						></Services>
+						<Resume
+							classicHeader={classicHeader}
+							darkTheme={darkTheme}
+						></Resume>
+						<Portfolio
+							classicHeader={classicHeader}
+							darkTheme={darkTheme}
+						></Portfolio>
+						<Contact
+							classicHeader={classicHeader}
+							darkTheme={darkTheme}
+						></Contact>
+					</div>
+					<Footer
+						classicHeader={classicHeader}
+						darkTheme={darkTheme}
+						handleNavClick={handleNavClick}
+					></Footer>
+				</div>
+				{/* back to top */}
+				<Tooltip text="Back to Top" placement="left">
+					<span
+						id="back-to-top"
+						className="rounded-circle"
+						style={{
+							display: scrollTopVisible ? "inline" : "none",
+						}}
+						onClick={() => {
+							window.scrollTo({ top: 0, behavior: "smooth" });
+						}}
+					>
+						<i className="fa fa-chevron-up"></i>
+					</span>
+				</Tooltip>
 
-        <TermsAndConditions darkTheme={darkTheme}></TermsAndConditions>
-        <Disclaimer darkTheme={darkTheme}></Disclaimer>
-      </div>
-    </>
-  );
+				<TermsAndConditions darkTheme={darkTheme}></TermsAndConditions>
+				<Disclaimer darkTheme={darkTheme}></Disclaimer>
+			</div>
+		</>
+	);
 }
 
 export default App;
