0593fdc0bb59c946971f87c495afc7fad27dee56
Daniel Yoo <dacapo0509@gmail.com>
2023-02-25 11:44:43 -0800

    help

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,11 +44,7 @@ const AboutUs = ({ classicHeader, darkTheme }) => {
 							Web Developer
 						</h2>
 						<p className={darkTheme ? "text-white-50" : ""}>
-							I help you build brand for your business at an
-							affordable price. Thousands of clients have procured
-							exceptional results while working with our dedicated
-							team. when an unknown printer took a galley of type
-							and scrambled it to make a type specimen book.
+							I used to make teeth. Now I make apps.
 						</p>
 						<p className={darkTheme ? "text-white-50" : ""}>
 							Delivering work within time and budget which meets
