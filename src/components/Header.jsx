39d5325628401a06b653f78e88e5905ad94ff0dc
Daniel Yoo <dacapo0509@gmail.com>
2023-06-07 13:17:50 -0700

    changes for icons in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -149,43 +149,21 @@ const Header = ({ classicHeader, darkTheme, homeRef, handleNavClick }) => {
 						</ul>
 					</div>
 					<ul className="social-icons social-icons-muted social-icons-sm mt-lg-auto ms-auto ms-lg-0 d-flex">
-						<li className="social-icons-facebook">
-							<Tooltip text="Facebook" placement="top">
+						<li className="social-icons-linkedin">
+							<Tooltip text="Linkedin" placement="top">
 								<a
-									href="http://www.facebook.com/"
+									href="https://www.linkedin.com/in/daniel-yoo-b385a6182/"
 									target="_blank"
 									rel="noopener noreferrer"
 								>
-									<i className="fab fa-facebook-f" />
-								</a>
-							</Tooltip>
-						</li>
-						<li className="social-icons-twitter">
-							<Tooltip text="Twitter" placement="top">
-								<a
-									href="http://www.twitter.com/"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<i className="fab fa-twitter" />
-								</a>
-							</Tooltip>
-						</li>
-						<li className="social-icons-dribbble">
-							<Tooltip text="Dribbble" placement="top">
-								<a
-									href="http://www.dribbble.com/"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<i className="fab fa-dribbble" />
+									<i className="fab fa-linkedin" />
 								</a>
 							</Tooltip>
 						</li>
 						<li className="social-icons-github">
-							<Tooltip text="Google" placement="top">
+							<Tooltip text="Github" placement="top">
 								<a
-									href="http://www.google.com/"
+									href="https://github.com/kympanic"
 									target="_blank"
 									rel="noopener noreferrer"
 								>
